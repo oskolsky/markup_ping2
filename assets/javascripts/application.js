354a8786a43815260717355a5c353c25182c9ffa
Maxim Oskolsky <mo@coalla.ru>
2013-09-04 17:53:56 +0400

    .

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -2,4 +2,8 @@ $(function() {
 
   $('.scroll-pane').jScrollPane();
 
+  $('.cycle-slideshow').on('cycle-after', function(event, optionHash, outgoingSlideEl, incomingSlideEl, forwardFlag) {
+    $('.scroll-pane').jScrollPane();
+  });
+
 });
