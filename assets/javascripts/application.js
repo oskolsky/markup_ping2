fe078535321db6c6ca6b7eb4c80a200e474466b3
Maxim Oskolsky <mo@coalla.ru>
2013-09-05 14:00:55 +0400

    .

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,10 +1,29 @@
 $(function() {
 
-  $('.scroll-pane').jScrollPane();
-
-  $('.cycle-slideshow').on('cycle-after', function(event, optionHash, outgoingSlideEl, incomingSlideEl, forwardFlag) {
-    $('.scroll-pane').jScrollPane();
+  var mainSlide = $('.slider').cycle({
+    fx: 'scrollHorz',
+    timeout: 0,
+    speed:   500,
+    slides:  '.page',
+    pager:   '.nav',
+    pagerTemplate: '',
+    centerHorz: true,
+    centerVert: true
   });
+
+  mainSlide.on('cycle-before', function(event, optionHash) {
+    $('.header_t').fadeOut().text();
+  });
+
+  mainSlide.on('cycle-after', function(event, optionHash) {
+    $('.scroll-pane').jScrollPane();
+    $('.header_t').text(optionHash.header).fadeIn();
+  });
+
+  var defaultHdr = $('.cycle-pager-active').data('header');
+  $('.header_t').text(defaultHdr).fadeIn();
+
+  $('.scroll-pane').jScrollPane();
 
   $(document).ready(function () {
     $("#background-image").fullscreenBackground();
