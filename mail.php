38ccc0b99316f8c197a439670c7122a8511f7f02
Maxim Oskolsky <mo@coalla.ru>
2013-09-18 12:25:43 +0400

    .

--- mail.php
+++ mail.php
@@ -8,7 +8,7 @@
     include_once ('lib/mail.class.php');
     $mail = new mail('utf-8');
     $mail -> To($to);
-    $mail -> Subject('Feedback from the site sky-cat.info: "'.$data['subject'].'"');
+    $mail -> Subject('Обратная связь с сайта pingapp.me: "'.$data['subject'].'"');
     $mail -> Body('Name: '.$data['name']."\n".'E-mail: '.$data['email']."\n".'Inquiry: '.$data['inquiry']."\n".'Message: '.$data['message']);
     $mail -> Priority(3);
     $mail -> Send();
